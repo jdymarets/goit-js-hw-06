78b89e14e955af497104250a008e0c8b86bc5727
jdymarets <91064865+jdymarets@users.noreply.github.com>
2022-02-01 16:01:28 +0200

    задание 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log('Number of categories', categoriesEl.length);
+
+categoriesEl.forEach(function(category) {
+    console.log('Category:', category.firstElementChild.textContent);
+    console.log('Element:', category.lastElementChild.children.length);
+});
+
+
