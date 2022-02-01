50c2eb04d777d7ad2b651fc935cd0f96ce9d4c7a
jdymarets <91064865+jdymarets@users.noreply.github.com>
2022-02-01 21:57:15 +0200

    задача 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEL = document.querySelector("ul");
+
+const makeElementList = names => {
+  return names.map(name => {
+    const navitemEl = document.createElement("li");
+    navitemEl.classList.add("item");
+    navitemEl.textContent = name;
+
+    return navitemEl;
+  });
+};
+const elements = makeElementList(ingredients);
+ingredientsEL.append(...elements);
