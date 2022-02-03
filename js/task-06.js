ef79466cdb3acc2740503beeac56a20940e528f7
jdymarets <91064865+jdymarets@users.noreply.github.com>
2022-02-03 10:53:23 +0200

    task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,18 +1,10 @@
-let inputVal = document.getElementById("validation-input");
-
-let totalLenght = inputVal.getAttribute("data-length");
-let intTotallenght = parseInt(totalLenght, 10);
-
-inputVal.oninput = function() {
-  if (inputVal.value.length === intTotallenght) {
-    inputVal.classList.remove("invalid");
-    inputVal.classList.add("valid");
+const inputEl = document.querySelector('input');
+inputEl.addEventListener('blur', event => {
+  if (event.currentTarget.value.length >= inputEl.dataset.length) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.add('invalid');
+    inputEl.classList.remove('valid');
   }
-  if (inputVal.value.length === 0) {
-    inputVal.classList.remove("valid");
-    inputVal.classList.remove("invalid");
-  }
-  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
-    inputVal.classList.add("invalid");
-  }
-};
+});
