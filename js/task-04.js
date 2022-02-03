4ba1113179d44b80d718a4782024a24d91aba92a
jdymarets <91064865+jdymarets@users.noreply.github.com>
2022-02-03 10:14:29 +0200

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,22 @@
-const button = {
-    sub: document.querySelector("[data-action='increment']"),
-    add: document.querySelector("[data-action='decrement']"),
-    span: document.querySelector("#value"),
-  };
+const counter = document.querySelector("#counter");
+const value = document.querySelector("#value");
+
+const decrement = counter.querySelector('[data-action = "decrement"]');
+const increment = counter.querySelector('[data-action = "increment"]');
+
+let counterValue = 0;
+
+
+const mouseClick = decrement.addEventListener("click", ()=>{
+  counterValue -=1;
+  // console.log("button decrement was clicked", counterValue);
+  value.textContent = counterValue;
   
-  let counterValue = 0;
-  const increment = () => {
-    counterValue += 1;
-  
-   button.span.textContent = counterValue;
-  };
-  
-  const decrement = () => {
-    counterValue -= 1;
-  
-    button.span.textContent = counterValue;
-  };
-  
-  button.sub.addEventListener("click", increment);
-  button.add.addEventListener("click", decrement);
+})
+
+const mouseClickI = increment.addEventListener("click", () =>{
+  counterValue +=1;
+  // console.log("button increment was clicked", counterValue);
+  value.textContent = counterValue;
+ 
+})
